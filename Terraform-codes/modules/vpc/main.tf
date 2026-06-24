186340ad62ae45dca02459e51a8f161fce1b8f9d
Rishikvashu <95581558+Rishikvashu@users.noreply.github.com>
2026-06-24 22:26:11 +0530

    Update main.tf

--- Terraform-codes/modules/vpc/main.tf
+++ Terraform-codes/modules/vpc/main.tf
@@ -1,7 +1,15 @@
-resource "aws_vpc" "a5_vpc" {
-    cidr_block = var.vpc_cidr
+resource "aws_vpc" "kafka_vpc" {
 
-    tags = {
-        Name = var.vpc_name
-    }
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+
+    Name        = var.vpc_name
+    Project     = "Kafka-Automation"
+    ManagedBy   = "Terraform"
+    Environment = "Dev"
+
+  }
 }
