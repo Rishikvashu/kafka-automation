bc08c229af83eedd474d9e30c642213b6a1664b0
Jenkins <jenkins@example.com>
2026-06-28 15:59:28 +0000

    .

--- Terraform-codes/modules/subnet/main.tf
+++ Terraform-codes/modules/subnet/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "kafka_subnet" {
 }
 resource "aws_route_table_association" "kafka_rta" {
 
-  subnet_id      = aws_subnet.kafka_subnet.id
+  subnet_id = aws_subnet.kafka_subnet.id
 
   route_table_id = var.route_table_id
 }
