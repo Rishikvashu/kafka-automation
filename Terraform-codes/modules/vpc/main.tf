321ac5d419054da02136463ab1def0706a80e4e6
Rishikvashu <95581558+Rishikvashu@users.noreply.github.com>
2026-06-28 17:35:57 +0530

    Update main.tf

--- Terraform-codes/modules/vpc/main.tf
+++ Terraform-codes/modules/vpc/main.tf
@@ -13,3 +13,28 @@ resource "aws_vpc" "kafka_vpc" {
 
   }
 }
+
+resource "aws_internet_gateway" "kafka_igw" {
+
+  vpc_id = aws_vpc.kafka_vpc.id
+
+  tags = {
+    Name = "kafka_igw"
+  }
+}
+
+resource "aws_route_table" "kafka_rt" {
+
+  vpc_id = aws_vpc.kafka_vpc.id
+
+  route {
+
+    cidr_block = "0.0.0.0/0"
+
+    gateway_id = aws_internet_gateway.kafka_igw.id
+  }
+
+  tags = {
+    Name = "kafka_rt"
+  }
+}
